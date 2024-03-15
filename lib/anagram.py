7eb8c20514699b0fa218ca79cc310415d5f8ed41
Koby Fink <finkkoby@icloud.com>
2024-03-15 15:53:06 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import ipdb
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+    def match(self, anagrams_list):
+        target_char_list = sorted(list(self.word))
+        answer_list = []
+        for str in anagrams_list:
+            anagram_char_list = sorted(list(str))
+            if anagram_char_list == target_char_list:
+                answer_list.append(str)
+        return answer_list
